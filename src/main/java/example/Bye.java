335d99b9b7a5cc599f56954cb9a00e7692bd231c
Anish Sana <anishsana91@gmail.com>
2017-07-03 09:25:05 -0400

    First commit

---
+++ src/main/java/example/Bye.java
@@ -0,0 +1,12 @@
+package example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Bye implements RequestHandler<String, String> {
+
+	public String handleRequest(String input, Context context) {
+		String output = "Bye, " + input + "!";
+		return output;
+	}
+}
